60e4d760252dc5ac9377acc9be1977580ace963c
Mario Cho <hephaex@gmail.com>
2018-02-19 23:27:40 +0900

    Create cifar100.py

---
+++ cifar100.py
@@ -0,0 +1,61 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.datasets import cifar100
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+from keras.optimizers import SGD
+
+batch_size = 128
+nb_classes = 100
+nb_epoch = 1
+
+(X_train, y_train), (X_test, y_test) = cifar100.load_data()
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train -= 128
+X_test -= 128
+X_train /= 128
+X_test /= 128
+
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+model = Sequential()
+
+model.add(Convolution2D(32, 3, 3, border_mode='same',
+                        input_shape=(3, 32, 32)))
+model.add(Activation('relu'))
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Convolution2D(64, 3, 3, border_mode='same'))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+# let's train the model using SGD + momentum (how original).
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd)
+
+model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch, show_accuracy=True, verbose=1, validation_data=(X_test, Y_test))
+model.save('cifar100.h5')
+score = model.evaluate(X_test, Y_test)
