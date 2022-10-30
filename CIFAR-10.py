cc0c8398cf27d445d3fa81455ed4a042c4e356d3
Mario Cho <hephaex@gmail.com>
2022-10-30 23:06:55 +0900

    Update CIFAR-10.py

--- CIFAR-10.py
+++ CIFAR-10.py
@@ -6,11 +6,12 @@ from torch.utils.data import Dataset,DataLoader
 import torchvision.datasets as dsets
 import matplotlib.pyplot as plt
 
-batch_size = 400
+batch_size = 800
 train_data = dsets.CIFAR10(root='./cifar-10', train=True, download=False, transform=transforms.Compose([transforms.RandomHorizontalFlip(p=0.5), transforms.ToTensor(), transforms.RandomErasing(p=0.5, scale=(0.02, 0.4), ratio=(0.33, 3.0))]))
 train_loader = DataLoader(train_data,batch_size=batch_size,shuffle=True)
 test_data = dsets.CIFAR10(root='./cifar-10', train=False, download=False, transform=transforms.Compose([transforms.ToTensor(),]))
 test_loader = DataLoader(test_data,batch_size=batch_size,shuffle=False)
+
 
 def image_show(data_loader,n):
 
